8abb594da09bcad47a9438e4bb08a720ca6f221f
Jack <94463439+Jack1521@users.noreply.github.com>
2022-01-08 04:30:56 +0330

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,9 +1,9 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '918396901628', 'kazukafu', m)
+  this.sendContact(m.chat, '989035024059', 'JACK', m)
 }
-handler.help = ['owner/creator']
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
 handler.command = /^(owner|creator)$/i
